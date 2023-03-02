b590a4c2f54441b2bbfe4a64cb058e113cb3f8fc
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-03-01 21:08:53 -0500

    Se crea el script y se salvan las bases de datos.

---
+++ 2. SCRIPTS/0_modificacion_bases_de_datos_formatoRDS.R
@@ -0,0 +1,22 @@
+#Problmem_Set_3
+# Transformación de bases de datos a formato Rds.
+#Carlos Vergara, Alexandra Rizo, Danna Bolaños, Héctor Tacumán
+#_______________________________________________________________
+
+#######Preparación del espacio########
+
+## Se llaman los paquetes para su uso en el Script:
+rm(list=ls())
+install.packages("pacman")
+require(pacman)
+p_load(tidyverse,rvest,writexl,rio,skimr,pastecs,PerformanceAnalytics,naniar,gtsummary)
+#Se llaman las bases de datos: 
+setwd("~/OneDrive - Universidad de los Andes/2023/2023-1/BIG DATA/TALLERES GRUPALES/TALLER No. 3/Problem_Set_3_Making_Money_with_ML/3. STORE")
+
+df_test <- import("test.csv")
+df_train <- import("train.csv")
+
+## Se salvan las bases de datos como rds.
+
+saveRDS(df_test,"df_test.rds")
+saveRDS(df_train,"df_train.rds")
