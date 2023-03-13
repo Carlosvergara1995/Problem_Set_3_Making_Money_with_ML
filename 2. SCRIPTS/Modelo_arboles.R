19339a93f4d2f6b6610394781b26401ed4292382
dannabn12 <dannab.n12@gmail.com>
2023-03-12 22:20:06 -0500

    Update Modelo_arboles.R

--- 2. SCRIPTS/Modelo_arboles.R
+++ 2. SCRIPTS/Modelo_arboles.R
@@ -1,10 +1,10 @@
 #Modelo Arboles
 set.seed(123)
 
-#importamos las bases finales 
+#importamos las bases 
 test_s <- readRDS("~/Desktop/git hut repositorios/Problem_Set_3_Making_Money_with_ML/3. STORE/test_s.rds")
 train_s <- readRDS("~/Desktop/git hut repositorios/Problem_Set_3_Making_Money_with_ML/3. STORE/train_s.rds")
-
+df_test <- readRDS("~/Desktop/git hut repositorios/Problem_Set_3_Making_Money_with_ML/3. STORE/df_test.rds")
 #divimos nuestras datos en train, validacion 
 inTrain <- createDataPartition(
   y = train_s$price,## La variable dependiente u objetivo 
@@ -13,7 +13,8 @@ inTrain <- createDataPartition(
 
 valit <- train_s[-inTrain,]
 train1 <- train_s[inTrain,]
-test <- test_s %>% mutate(price = NULL)
+test <- test_s %>% mutate(price = NULL, property_id=df_test$property_id)
+
 
 # Creamos las particiones para hacer validación cruzada
 cv10 <- trainControl(number = 10, method = "cv")
@@ -29,6 +30,8 @@ modeloa <- train(price ~ .,
 
 fancyRpartPlot(modeloa$finalModel)
 #enontramos que la variable bathrooms predice bien los resultados
+
+
 
 #analizamos las predicciones del modelo
 y_hat_insample1 = predict(modeloa, newdata = train1)
@@ -75,6 +78,7 @@ ggplot(modelob$results, aes(x = min.node.size, y = RMSE,
 #analizamos este modelo dentro y fuera de muestra
 y_hat_insample2 = predict(modelob, newdata = train1)
 y_hat_outsample2 = predict(modelob, newdata = valit)
+y_hat_outsample3 = predict(modelob, newdata = test)
 
 MAPE(y_pred = y_hat_insample2, y_true = train1$price)
 MAE(y_pred = y_hat_insample2, y_true = train1$price)
@@ -83,7 +87,55 @@ MAPE(y_pred = y_hat_outsample2, y_true = valit$price)
 MAE(y_pred = y_hat_outsample2, y_true = valit$price)
 
 #
-MAPE(y_pred = y_hat_outsample2, y_true = test$price)
-MAE(y_pred = y_hat_outsample2, y_true = test$price)
+
+Modelo1_clasificación <- data.frame( property_id= test$property_id, price =  y_hat_outsample3)
+write.csv(Modelo1_clasificación, "Modelo1_clasificación.csv", row.names = FALSE)
+
+#################
+
+
+
+#Ramdom Forest####
+
+#creamos una grilla
+
+modeloc <- train(price~.,
+                 data=train1,
+                 trControl= cv10,
+                 metric ="RMSE",
+                 tuneGrid=grilla,
+                 method = "ranger")
+
+
+# graficamos
+ggplot(modeloc$results, aes(x = min.node.size, y = RMSE, 
+                            color = as.factor(mtry))) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Resultados del grid search",
+       x = "Mínima cantidad de observaciones por hoja",
+       y = "RMSE (Cross-Validation)") +
+  scale_color_discrete("Número de predictores seleccionados al azar") +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+
+#encontramos que el modelo optimo tiene una cantidad optima de 
+#variables con un aproximado de  nodos
+
+#analizamos este modelo dentro y fuera de muestra
+y_hat_insample3 = predict(modeloc, newdata = train1)
+y_hat_outsample3 = predict(modeloc, newdata = valit)
+y_hat_outsample4 = predict(modeloc, newdata = test)
+
+MAPE(y_pred = y_hat_insample3, y_true = train1$price)
+MAE(y_pred = y_hat_insample3, y_true = train1$price)
+
+MAPE(y_pred = y_hat_outsample3, y_true = valit$price)
+MAE(y_pred = y_hat_outsample3, y_true = valit$price)
+
+
+
+
 
 
